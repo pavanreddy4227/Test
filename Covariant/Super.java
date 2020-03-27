815a7e3fb1c2c5df168192b87ea4feb95f6418ae
pavanreddy4227 <54386960+pavanreddy4227@users.noreply.github.com>
2020-03-27 17:31:14 -0500

    Added

---
+++ Covariant/Super.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Covariant;
+
+/**
+ *
+ * @author Pavan Sai Kumar Reddy Kamjula
+ */
+public class Super {
+    Super get() {
+      System.out.println("This is Super Class");
+      return this;
+    }
+}
---
+++ static_and_private/Super.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package static_and_private;
+
+/**
+ *
+ * @author Pavan Sai Kumar Reddy Kamjula
+ */
+class Super {
+     private void display() {
+      System.out.println("Super class");    
+   }
+     public void print() {
+      System.out.println("Super class");    
+   }
+}
